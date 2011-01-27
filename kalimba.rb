728dcc7909a1e9f76b30b157b1c41cd9f6101edb
Bob Corsaro <bob@embed.ly>
2011-01-27 17:38:46 -0500

    Adds google analytics

--- kalimba.rb
+++ kalimba.rb
@@ -196,6 +196,18 @@ module Kalimba::Views
             });
           END
         end
+        script do
+          self <<<<-'END'
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-15045445-6']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+              var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+              ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+         END
       end
 
       body do
